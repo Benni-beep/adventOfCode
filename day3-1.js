2dd181cc6af3863ab6eeaeea98d915b141abb1b5
Benni-beep <krikelkr4kel@gmail.com>
2020-04-28 17:03:14 +0200

    Day3-1 solved

--- day3-1.js
+++ day3-1.js
@@ -1,37 +1,74 @@
 // https://adventofcode.com/2019/day/3
 
-const wireArray1 = require('./day3-1data.json')
-const wireArray2 = require('./day3-2data.json')
+const wireArray1 = require('./day3-1data-1.json')
+const wireArray2 = require('./day3-1data-2.json')
+// const wireArray1 = ["R75","D30","R83","U83","L12","D49","R71","U7","L72","U62","R66","U55","R34","D71","R55","D58","R83"]
+// const wireArray2 = ["L10","U10","R20","D50","R10","U50","L98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51","U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]
 
-/**
- * wo kreuzen sich die beiden Linien?
- * welche Kreuzung hat den geringsten Abstand zum Ausgangspunkt?
- * function distance(???)
- * if (line1.charAt(0) =="R"){x+}
- * if (line1.charAt(0) =="L"){x-}
- * if (line1.charAt(0) =="U"){y+}
- * if (line1.charAt(0) =="D"){y-}
- * 
- * für jede Linie Array mit allen Koordinaten erstellen
- * prüfen welche Koordinaten kommen in beiden Arrays vor -> const intersections = []
- * prüfen welche dieser gemeinsamen Koordinaten ist am nächsten am Ausgangspunkt
- */
+function stringToNumber(str) {
+    return parseInt(str.slice(1), 10)
+}
 
- for (let direction of wireArray1) {
-     let coordinates = [];
-     let x = 0;
-     let y = 0;
-     switch (direction.charAt(0)){
-         case "R":
-             coordinates.push({x: x + direction.slice(1)})
-             // wie koordination speichern/welche syntax? array oder object?
-             // muss ich format von direction.slice(1) von von string auf {number} ändern?
-         break;
-         case "L":
-         break;
-         case "U":
-         break;
-         case "D":
-         break;
-     };
- };
+function coordinates(arr) {
+
+    let coord = [];
+    let x = 0;
+    let y = 0;
+
+    for (let direction of arr) {
+
+        const range = stringToNumber(direction.slice(0));
+
+        switch (direction.charAt(0)) {
+            case "R":
+                for (let i = 1; i <= range; i++){
+                coord.push({ x: x += 1, y: y += 0 });
+            }
+                break;
+            case "L":
+                for (let i = 1; i <= range; i++){
+                    coord.push({ x: x -= 1, y: y += 0 });
+                }
+                break;
+            case "U":
+                for (let i = 1; i <= range; i++){
+                    coord.push({ x: x += 0, y: y += 1 });
+                }
+                break;
+            case "D":
+                for (let i = 1; i <= range; i++){
+                    coord.push({ x: x += 0, y: y -= 1 });
+                }
+                break;
+        };
+    };
+    return coord;
+}
+
+const coordinatesWire1 = coordinates(wireArray1);
+const coordinatesWire2 = coordinates(wireArray2);
+
+function intersections(arr1, arr2) {
+    let intersec = [];
+    for (let coordArr1 of arr1) {
+        for (let coordArr2 of arr2) {
+            if (coordArr1.x == coordArr2.x == true && coordArr1.y == coordArr2.y == true) { intersec.push(coordArr1) };
+        };
+    };
+    return intersec;
+};
+
+const intersectionArray = intersections(coordinatesWire1, coordinatesWire2);
+
+function distance(arr){
+    let distance = 0;
+    let distanceArray = [];
+    for (let intersection of arr){
+        distance = Math.abs(intersection.x) + Math.abs(intersection.y)
+        distanceArray.push(distance);
+    }
+    return distanceArray;
+}
+const distanceArray = distance(intersectionArray);
+
+console.log(Math.min.apply(null, distanceArray));
--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-const string = "R12424"
-console.log(string.slice(1))
+const a = {x: 12, y: 15};
+const b = {x: 12, y: 15};
+const c = {x: 13, y: 15};
+let array = [];
+
+if (a.x == b.x && a.y == b.y){console.log("yay")}
+
+array.push(a)
+console.log(array)
